413873b061a6e4df3677aa3b3de4b01c9b92f85c
benoitpetit <110.benp@protonmail.com>
2020-12-04 23:57:55 +0100

    deploy 04-12-2020-23-57

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => (
         <div className="row container-about p-3">
             <p className="text-justify" >J'ai 32 ans. Je suis <strong>Concepteur et Développeur d'application</strong> PHP/Symfony et React, je suis diplômé de 
             la Webforce3 de Lille où j’ai étudié l'intégration et le développement d'appplication Web et Mobile. 
-            J’effectue continuellement une veille informatique pour me tenir au courant des avancées technologiques et langages informatiques.. <br /><br />
+            J’effectue continuellement une veille informatique pour me tenir au courant des avancées technologiques et langages informatiques.<br /><br />
             Je suis une formation de perfectionnement chez ENI "Ecole d'informatique" dans la <strong>conception d'application : </strong>
             <blockquote className="header-cite">je suis à la recherche d'un stage pour finaliser cette formation (du 21 juin au 13 août 2021).</blockquote></p>
         </div>
